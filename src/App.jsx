3478e5473027f0e8952c5ffd17bc9c568651797d
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-09-23 21:04:39 +0400

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/users'
-        );
-        const data = await response.json();
-        setUsers(data);
-        if (!response.ok) throw new Error('Network response was not ok');
-      } catch (error) {
-        console.error('This is my Error: ', error);
-      }
-    };
-    fetchUsers();
-  }, []);
-
-  const fetchUserById = async (id) => {
-    try {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${id}`
-      );
-      const data = await response.json();
-      setUser(data);
-      if (!response.ok) throw new Error('Network response was not ok');
-    } catch (error) {
-      console.error('This is my Error: ', error);
-    }
-  };
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <h1>User List</h1>
-        <ul>
-          {users.map((user) => {
-            return (
-              <li
-                key={user.id}
-                onClick={() => {
-                  fetchUserById(user.id);
-                }}
-              >
-                {user.name}
-              </li>
-            );
-          })}
-        </ul>
-        {user && (
-          <div>
-            <h2>user Data</h2>
-            <p>
-              <strong>Name:</strong> {user.name}
-            </p>
-            <p>
-              <strong>Email:</strong> {user.email}
-            </p>
-            <p>
-              <strong>Address:</strong> {user.address.city}{' '}
-              {user.address.street}
-            </p>
-          </div>
-        )}
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
